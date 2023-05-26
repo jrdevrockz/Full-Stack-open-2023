ab500c57e721c1615e45178aec6eeb7054d81932
jrdevrockz <134650043+jrdevrockz@users.noreply.github.com>
2023-05-27 01:11:29 +0300

    Updated PORT variable.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,10 +7,10 @@ morgan.token('body', function jsonBody(res) {
   return JSON.stringify(res.body);
 });
 
-app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('build'));
 
 let persons = [
     { 
@@ -98,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
